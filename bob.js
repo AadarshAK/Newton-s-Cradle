7dd1319f99a63d2a28c412b642d6fd69137fb77c
AadarshAK <73172401+AadarshAK@users.noreply.github.com>
2020-12-25 11:27:18 +0530

    Add files via upload

---
+++ bob.js
@@ -0,0 +1,28 @@
+class Bob{
+   constructor(x,y,radius){
+      var options={
+       'restitution':0.8,
+       'friction' : 0.3,
+       'density':0.2,
+      }
+
+      this.body=Bodies.circle(mouseX,mouseY,radius,options);
+      this.radius=radius;
+      this.image=loadImage("stone.png");
+      World.add(world,this.body);
+   }
+
+   display(){
+
+       push();
+       var pos=this.body.position;
+       var angle=this.body.angle;
+       translate(pos.x,pos.y);
+       rotate(angle);
+       fill("white");     
+       imageMode(RADIUS);
+       image(this.image,0,0,this.radius,this.radius);
+       pop();
+   }
+}
+
